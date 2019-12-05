f5e14899553356fca24cd684b19b2bbc903bf65a
Gregory Belevskiy <gregory664@yandex.ru>
2019-12-05 16:55:38 +0300

    add bubble sort

---
+++ src/test/java/ru/learnJava/SortsTest.java
@@ -0,0 +1,32 @@
+package ru.learnJava;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.Arrays;
+
+import static org.junit.Assert.*;
+
+public class SortsTest {
+    private int[] array;
+    private int[] sortedArray = {1, 2, 3, 4, 5};
+
+    @Before
+    public void setUp() {
+        array = new int[] {5, 4, 3, 2, 1};
+    }
+
+    @Test
+    public void bubbleSort() {
+        Sorts.bubbleSort(array);
+        assertArrayEquals(array, sortedArray);
+    }
+
+    @Test
+    public void selectSort() {
+    }
+
+    @Test
+    public void insertSort() {
+    }
+}
